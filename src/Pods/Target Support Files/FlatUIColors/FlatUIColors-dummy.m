64124fdac041969a53448e745c8b23e368bed17a
Manuel Alejandro <marosabal@icloud.com>
2016-09-21 20:39:38 -0400

    Local Profile, Tapa Profile

--- src/Pods/Target Support Files/FlatUIColors/FlatUIColors-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FlatUIColors : NSObject
-@end
-@implementation PodsDummy_FlatUIColors
-@end
--- src/Pods/Target Support Files/FlatUIColors/FlatUIColors-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double FlatUIColorsVersionNumber;
-FOUNDATION_EXPORT const unsigned char FlatUIColorsVersionString[];
-
